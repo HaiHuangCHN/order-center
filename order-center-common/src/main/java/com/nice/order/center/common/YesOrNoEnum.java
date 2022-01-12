037d07eb4760a4e6372701eb7db74df49b317048
黄海 <hai.huang.a@outlook.com>
2022-01-12 19:45:04 +0800

    Update

---
+++ order-center-common/src/main/java/com/nice/order/center/common/YesOrNoEnum.java
@@ -0,0 +1,31 @@
+package com.nice.order.center.common;
+
+/**
+ * TODO
+ */
+public enum YesOrNoEnum {
+
+
+    YES(1, "是"),
+
+    NO(0, "否");
+
+    private Integer code;
+
+    private String remark;
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public String getRemark() {
+        return this.remark;
+    }
+
+    YesOrNoEnum(Integer code, String remark) {
+        this.code = code;
+        this.remark = remark;
+    }
+
+
+}
---
+++ order-center-common/src/test/java/com/nice/order/center/common/util/JsonUtilsTest.java
@@ -0,0 +1,35 @@
+package com.nice.order.center.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TODO Fill in desc
+ *
+ * @author haihuang95@zto.com
+ * @version 0.1.0
+ * @date 2021/11/11 16:22
+ * @since 0.1.0
+ */
+public class JsonUtilsTest {
+
+
+    @Test
+    public void testJson() {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("CHARGE", 0);
+        hashMap.put("ONLINE_PAYMENT", 0);
+        hashMap.put("BALANCE_PAYMENT", 0);
+        hashMap.put("REFUND", 5);
+        System.out.println(JsonUtils.objectToJsonCamel(hashMap));
+        HashMap map = JsonUtils.jsonToObject("{\"CHARGE\":0,\"ONLINE_PAYMENT\":0,\"BALANCE_PAYMENT\":0,\"REFUND\":5}", HashMap.class);
+        System.out.println(map);
+        System.out.println(map.get("CHARGE"));
+        System.out.println(map.get("REFUND"));
+        System.out.println(map.get("unknown"));
+    }
+
+
+}
