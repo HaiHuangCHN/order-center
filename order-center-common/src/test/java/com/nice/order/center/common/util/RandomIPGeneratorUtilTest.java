53f256b18e6ab0dafe84f76ebf8cda3d3914c6a5
黄海 <hai.huang.a@outlook.com>
2022-10-20 13:59:14 +0800

    Util

---
+++ order-center-common/src/test/java/com/nice/order/center/common/util/RandomIPGeneratorUtilTest.java
@@ -0,0 +1,24 @@
+package com.nice.order.center.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+/**
+ * 随机IP生成器
+ *
+ * @author haihuang95@zto.com
+ * @date 2022/9/18 19:19
+ */
+public class RandomIPGeneratorUtilTest {
+
+    @Test
+    public void testRandomIp() {
+        while (true) {
+            RandomIPGeneratorUtil.getRandomIp();
+        }
+    }
+
+}
