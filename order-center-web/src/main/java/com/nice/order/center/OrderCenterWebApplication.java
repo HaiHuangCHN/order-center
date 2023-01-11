ccccaedb59e60a0915276586bb686b13e98982fe
黄海 <hai.huang.a@outlook.com>
2023-01-11 13:32:42 +0800

    Update

--- order-center-web/src/main/java/com/nice/order/center/OrderCenterWebApplication.java
+++ order-center-web/src/main/java/com/nice/order/center/OrderCenterWebApplication.java
@@ -3,7 +3,6 @@ package com.nice.order.center;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(value = "com.nice.order.center.dao.mapper")
