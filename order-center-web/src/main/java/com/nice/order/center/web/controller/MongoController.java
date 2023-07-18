2d45862446e7c4bf92aebf6c6965b065e459f845
黄海 <hai.huang.a@outlook.com>
2023-07-18 15:24:10 +0800

    第二种：Service 层注入 MongoTemplate

--- order-center-web/src/main/java/com/nice/order/center/web/controller/MongoController.java
+++ order-center-web/src/main/java/com/nice/order/center/web/controller/MongoController.java
@@ -1,14 +1,20 @@
 package com.nice.order.center.web.controller;
 
+import com.nice.order.center.dao.entity.mongo.OrderDetail;
 import com.nice.order.center.service.dto.res.OrderDetailResDTO;
 import com.nice.order.center.service.service.order.OrderDetailMongoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author hai.huang.a@outlook.com
@@ -19,15 +25,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 @Controller
 @Deprecated
-@RequestMapping("mongo")
+@RequestMapping("mongo/test")
 public class MongoController {
 
     private final OrderDetailMongoService orderDetailMongoService;
 
-    @GetMapping("/testQueryMongo")
+    private final MongoTemplate mongoTemplate;
+
+    @GetMapping("/repository")
     @ResponseBody
-    public OrderDetailResDTO testRedisAtomicInteger(@RequestParam("userNo") String userNo) {
+    public OrderDetailResDTO repository(@RequestParam("userNo") String userNo) {
         return orderDetailMongoService.findOrderDetailByUserNo(userNo);
+    }
+
+    @GetMapping("/mongo/template")
+    @ResponseBody
+    public List<OrderDetail> mongoTemplate(@RequestParam("userNo") String userNo) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("userNo").is(userNo));
+        return mongoTemplate.find(query, OrderDetail.class);
     }
 
 }
